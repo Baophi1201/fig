b2ba5cdb2404be3c69a03b1ea8d84d6fcf1e8bf0
Baophi1201 <xgusper@gmail.com>
2026-05-05 22:55:33 +0700

    change auto update from git to zip

--- src/utils/version_check.py
+++ src/utils/version_check.py
@@ -16,7 +16,7 @@ import requests
 from packaging import version
 
 # ===================== CẤU HÌNH =====================
-CURRENT_VERSION = "1.0.7"
+CURRENT_VERSION = "1.0.8"
 
 # URL raw của file version.json trong repo GitHub
 VERSION_URL = "https://raw.githubusercontent.com/Baophi1201/fig/main/version.json"
